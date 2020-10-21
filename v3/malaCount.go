b6e5e67ca14518935193ed95377e13290e8dde3d
Jiří Martínek <zwo@centrum.cz>
2020-10-21 15:27:29 +0200

    Added curly brace

--- v3/malaCount.go
+++ v3/malaCount.go
@@ -1,8 +1,9 @@
 package arithmetic
+
 // Counts number of malas to achieve given repetitions
 func MalaCounts(n int) (int, error) {
-    if n % 108 != 0 {
-        return -1, errors.New("Repetitions not divisible by 108!")
-    }
-    return n / 108, nil
-
+	if n%108 != 0 {
+		return -1, errors.New("Repetitions not divisible by 108!")
+	}
+	return n / 108, nil
+}
